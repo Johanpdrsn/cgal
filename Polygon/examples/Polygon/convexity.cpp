83a735ab8ef94f0babebd309ec45711cd5b0bdf9
Johan Pedersen <johan-p@live.dk>
2022-03-17 16:33:48 +0100

    Resolve lazy NT tree in for loop to avoid stack overflow

--- Polygon/examples/Polygon/convexity.cpp
+++ Polygon/examples/Polygon/convexity.cpp
@@ -6,11 +6,11 @@
 #include <CGAL/Polygon_2_algorithms.h>
 #include <CGAL/Constrained_triangulation_2.h>
 #include <CGAL/Triangulation_face_base_with_info_2.h>
+#include <CGAL/Cartesian.h>
+
 #include <fstream>
 #include <random>
 
-//#include <CGAL/Cartesian.h>
-//typedef CGAL::Cartesian<double> Kernel;
 typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
 typedef CGAL::Polygon_2<Kernel> Polygon_2;
 typedef Kernel::Triangle_2 Triangle_2;
@@ -86,20 +86,17 @@ void mark_domains(CT &cdt) {
     }
 }
 
-std::vector<Triangle_2> triangulate(Polygon_2 &polygon) {
+void triangulate(Polygon_2 &polygon, std::vector<Triangle_2> &triangles) {
 
     CT ct;
     ct.insert_constraint(polygon.vertices_begin(), polygon.vertices_end(), true);
     mark_domains(ct);
 
-    std::vector<Triangle_2> triangles;
     for (auto var: ct.finite_face_handles()) {
         if (var->info().in_domain()) {
             triangles.emplace_back(ct.triangle(var));
         }
     }
-
-    return triangles;
 }
 
 void read_polygon(const std::string &fileName, Polygon_2 &polygon) {
@@ -145,10 +142,11 @@ Kernel::FT two_point_visibility_sample(const Polygon_2 &polygon,
                                        CGAL::Random_points_in_triangles_2<Point_2> &triangle_point_generator,
                                        const Kernel::FT &n) {
 
-    Kernel::FT sum = 0;
+    int sum = 0;
+    Point_2 p1, p2;
     for (int i = 0; i < n; i++) {
-        Point_2 p1 = *triangle_point_generator++;
-        Point_2 p2 = *triangle_point_generator++;
+        p1 = *triangle_point_generator++;
+        p2 = *triangle_point_generator++;
 
 //        Triangle_2 A3 = Triangle_2{Point_2{1, 2}, Point_2{0, 0}, Point_2{1, 0.5}};
 //        Triangle_2 A1a = Triangle_2{Point_2{1, 0.5}, Point_2{0, 0}, Point_2{1, -1}};
@@ -166,7 +164,7 @@ Kernel::FT two_point_visibility_sample(const Polygon_2 &polygon,
         }
     }
 
-    return (n - sum) / n;
+    return ((n - sum) / n).exact();
 }
 
 Kernel::FT visibility_polygon_sample(const Polygon_2 &polygon,
@@ -174,7 +172,7 @@ Kernel::FT visibility_polygon_sample(const Polygon_2 &polygon,
                                      const Kernel::FT &n) {
     typedef CGAL::Arr_segment_traits_2<Kernel> Traits_2;
     typedef CGAL::Arrangement_2<Traits_2> Arrangement_2;
-    typedef CGAL::Triangular_expansion_visibility_2<Arrangement_2, CGAL::Tag_false> RSPV;
+    typedef CGAL::Triangular_expansion_visibility_2<Arrangement_2> TEV;
 
 //    Triangle_2 A3 = Triangle_2{Point_2{1, 2}, Point_2{0, 0}, Point_2{1, 0.5}};
 //    Triangle_2 A1a = Triangle_2{Point_2{1, 0.5}, Point_2{0, 0}, Point_2{1, -1}};
@@ -183,19 +181,22 @@ Kernel::FT visibility_polygon_sample(const Polygon_2 &polygon,
 
     Arrangement_2 env;
     CGAL::insert(env, polygon.edges_begin(), polygon.edges_end());
-    RSPV regular_visibility{env};
+    TEV regular_visibility{env};
 
     Arrangement_2::Face_const_handle *face;
     CGAL::Arr_naive_point_location<Arrangement_2> pl{env};
     CGAL::Arr_point_location_result<Arrangement_2>::Type obj;
     Arrangement_2 regular_output;
 
+    Point_2 point_sample;
+
+    Kernel::FT area = 0;
     Kernel::FT sum = 0;
 
     for (int i = 0; i < n; i++) {
         // find the face of the query point
         // (usually you may know that by other means)
-        Point_2 point_sample = *point_generator++;
+        point_sample = *point_generator++;
 //        Point_2 point_sample = kraemer_method(A3);
 
         obj = pl.locate(point_sample);
@@ -207,17 +208,16 @@ Kernel::FT visibility_polygon_sample(const Polygon_2 &polygon,
         regular_visibility.compute_visibility(point_sample, *face, regular_output);
 
         std::vector<Point_2> visible_points;
-        for (const auto &vertex: regular_output.vertex_handles()) {
-            visible_points.emplace_back(vertex->point());
-        }
+        std::transform(regular_output.vertices_begin(), regular_output.vertices_end(),
+                       std::back_inserter(visible_points), [](const auto &x) -> Point_2 { return x.point(); });
 
-        Kernel::FT area{};
-        CGAL::area_2(visible_points.begin(), visible_points.end(), area);
+        area = CGAL::polygon_area_2(visible_points.begin(), visible_points.end(), Kernel());
 
         sum += CGAL::abs(area);
+        sum = sum.exact();
     }
 
-    return sum / (polygon.area() * n);
+    return (sum / (polygon.area() * n)).exact();
 }
 
 
@@ -235,8 +235,10 @@ Kernel::FT simulate(const std::function<Kernel::FT(
 
 //    read_polygon(fileName, polygon);
 
-    const std::vector<Triangle_2> triangles = triangulate(polygon);
+    std::vector<Triangle_2> triangles;
+    triangulate(polygon, triangles);
     CGAL::Random_points_in_triangles_2<Point_2> triangle_point_generator{triangles};
+
 
     return vis_func(polygon, triangle_point_generator, n);
 }
@@ -244,19 +246,11 @@ Kernel::FT simulate(const std::function<Kernel::FT(
 
 int main() {
     const Kernel::FT N = 100000;
-    const std::string fileName{"rpg.line"};
-
-    CGAL::set_pretty_mode(cout);
+    const std::string fileName{"many.line"};
 
     std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double, std::milli>> t1;
     std::chrono::time_point<std::chrono::steady_clock, std::chrono::duration<double, std::milli>> t2;
     std::chrono::duration<double, std::milli> ms_double{};
-
-    t1 = std::chrono::high_resolution_clock::now();
-    Kernel::FT polygon = simulate(visibility_polygon_sample, fileName, N);
-    t2 = std::chrono::high_resolution_clock::now();
-    ms_double = t2 - t1;
-    cout << "Polygon prob: " << polygon << " in " << ms_double.count() << "ms" << endl;
 
     t1 = std::chrono::high_resolution_clock::now();
     Kernel::FT two_points = simulate(two_point_visibility_sample, fileName, N);
@@ -264,5 +258,11 @@ int main() {
     ms_double = t2 - t1;
     cout << "Two points prob: " << two_points << " in " << ms_double.count() << "ms" << endl;
 
-    return 0;
+    t1 = std::chrono::high_resolution_clock::now();
+    Kernel::FT polygon = simulate(visibility_polygon_sample, fileName, N);
+    t2 = std::chrono::high_resolution_clock::now();
+    ms_double = t2 - t1;
+    cout << "Polygon prob: " << polygon << " in " << ms_double.count() << "ms" << endl;
+
+    return EXIT_SUCCESS;
 }
